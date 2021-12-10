5cc5bdf8c2ca8fe5604b6f3b2d789e0233ddef8b
Bhavya Sree <bhavyasreekolluri3@gmail.com>
2021-12-10 19:06:09 +0530

    updated

--- Guessgame.py
+++ Guessgame.py
@@ -4,20 +4,27 @@ user_name = input('Good name,Please! : ')
 print('HELLO!' , user_name)
 words = ['grapes' , 'strawberry' , 'watermelon' , 'kiwi' ,'orange']
 c = random.choice(words)
-print(c)
+guesses = ''
 user_turns = 12
 while user_turns > 0:
     failed = 0
-    user_choice = input('Enter your character : ')
-    if user_choice in c:
-        print(user_choice,"it's there")
-    else:
-        print('wrong')
-        failed += 1
-        print(failed,'you have')
-    
-    continue  
-    
+    for char in c:
+        if char in guesses:
+            print(char)
+        else:
+            print('-')
+            failed += 1
+    if failed == 0:
+        print('You win')
+        print('the word is',c)
+        break
+    guess = input("guess the character: ")
+    guesses += guess
+    if guess not in c:
+        user_turns -= 1
+        print("Soory! It's wrong")
+        print('You have',user_turns,'Chances')
+        if user_turns==0:
+            print('you LOSE')
 
-    
-    
+
