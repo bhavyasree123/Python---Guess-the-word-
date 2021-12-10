3673d4b5a37b24c3c2cb23f972a9ddccb3e67743
Bhavya Sree <bhavyasreekolluri3@gmail.com>
2021-12-10 17:44:35 +0530

    updated

--- Guessgame.py
+++ Guessgame.py
@@ -1,1 +1,20 @@
-print('super girl bhavya ')
+import random
+
+user_name = input('Good name,Please! : ')
+print('HELLO!' , user_name)
+words = ['grapes' , 'strawberry' , 'watermelon' , 'kiwi' ,'orange']
+c = random.choice(words)
+print(c)
+while True:
+    user_choice = input('Enter your character : ')
+    for word in c:
+        print(word)
+    if user_choice in c:
+        print(user_choice,"it's there")
+    else:
+        print('wrong')
+    user_turns = 12
+    if user_turns <= 12:
+        continue
+    elif user_turns > 12:
+        quit()
