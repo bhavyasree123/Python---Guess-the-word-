92688bc01deb4871d4b6582d703397a24ebb8568
Bhavya Sree <bhavyasreekolluri3@gmail.com>
2021-12-10 18:23:30 +0530

    updated

--- Guessgame.py
+++ Guessgame.py
@@ -5,16 +5,19 @@ print('HELLO!' , user_name)
 words = ['grapes' , 'strawberry' , 'watermelon' , 'kiwi' ,'orange']
 c = random.choice(words)
 print(c)
-while True:
+user_turns = 12
+while user_turns > 0:
+    failed = 0
     user_choice = input('Enter your character : ')
-    for word in c:
-        print(word)
     if user_choice in c:
         print(user_choice,"it's there")
     else:
         print('wrong')
-    user_turns = 12
-    if user_turns <= 12:
-        continue
-    elif user_turns > 12:
-        quit()
+        failed += 1
+        print(failed,'you have')
+    
+    continue  
+    
+
+    
+    
